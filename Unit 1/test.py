cb90de372a13d9fa221160bea6c1052ebc5cd8a0
Walker <walkerd24@greenhill.org>
2020-09-10 10:27:46 -0500

    Hw test code

--- Unit 1/test.py
+++ Unit 1/test.py
@@ -1,4 +1,18 @@
-x= 3
-y=7
-y = 3+ x
-print (y)
+counter = 100
+miles   = 1000.0
+name    = "John"
+
+print (counter)
+print (miles)
+print (name)
+
+print ("---------------------")
+
+str = 'Hello World!'
+
+print (str)
+print (str[0])
+print (str[2:5])
+print (str[2:])
+print (str * 2)
+print (str + "TEST")
