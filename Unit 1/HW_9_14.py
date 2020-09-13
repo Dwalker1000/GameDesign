f2f10a90f67620a0000301835c39c5f4db54107e
Walker <walkerd24@greenhill.org>
2020-09-13 12:37:13 -0500

    hw for 9/14/2020

---
+++ Unit 1/HW_9_14.py
@@ -0,0 +1,26 @@
+flo = 31.0
+greeting = "Hello"
+age = 15
+com = 1j
+bool = True
+
+print (type(flo))
+print (type(greeting))
+print (type(age))
+print (type(com))
+print (type(bool))
+
+x = flo - age
+print (x)
+
+x = flo + age
+print (x)
+
+x = flo*age
+print (x)
+
+x = flo/age
+y= flo%age
+print (int(x))
+print ('modulus = ' + str(y))
+print ('The modulus of two varuables is the remainder if a number has no remainder then the modulus should be 0. You ues the % sign to find the modulus.')
