896772915232571a7febe990f4ac5deb6db26a24
Walker <walkerd24@greenhill.org>
2020-09-14 11:38:25 -0500

    class work

---
+++ Unit 1/branching1.py
@@ -0,0 +1,23 @@
+#Daniel Walker
+#this program is to learn how to branch
+#9/14/2020
+
+#declare variables
+a = 7
+b = 13
+c = 14
+d = 13
+if a%2 == 0:
+    print (b+c)
+elif b%5 == 0:
+    print (c-d)
+else:
+    print(d*c/a)
+# else:
+#     print('a is odd')
+#
+# if b%2 == 0:
+#     print (c-d)
+#
+# else:
+#     print(d*c/a)
