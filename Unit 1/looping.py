81967e8da63496b7cd6c5a8262e4b7234930d0c6
Dwalker1000 <25walkerd@gmail.com>
2020-09-23 12:58:29 -0500

    update

--- Unit 1/looping.py
+++ Unit 1/looping.py
@@ -1,61 +1,53 @@
-# for x in range(1,10):
-#     print (x,end =" ")
-# print("")
-#
-# for x in range(1,5):
-#     print (x)
-# print("")
-#
-# #nested loops
-# for line in range(1,10):
-#     for x in range(line):
-#         print (line, end = ' ')
-#     print("")
-# #controls where the pryrimid will start at
-# x=0
-# #controls where it wil finish
-# #will break if number is over 10
-# y=10
-# for line in range(x,y):
-#     #mkae sure each number is on a diffrent line
-#     print()
-# #both loops uses line to control flow
-#     for space in range (9-line):
-#         #prints spaces on first side
-#         print(" ",end='')
-#     for number in range (line):
-#         #prints the first side numbers
-#         print (line,end ='')
-#     #prints space in between both sides
-#     print (' ',end ='')
-#     for number in range(line):
-#         #prints the second set of numbers
-#         print (line, end = '')
-# print()
-# print()
+for x in range(1,10):
+    print (x,end =" ")
+print("")
 
-top= 5
-for i in range (0,top + 1):
-    for x in range (top-i, 0,-1):
-        print(x, end = ' ')
-    print()
+for x in range(1,5):
+    print (x)
+print("")
 
-start = 25
-end = 50
-for i in range(start,end):
-    if i>1:
-        for j in range (2,i):
-            if(i % j==0):
-                break
-        else:
-            print(i,end = ' ')
-print()
-print()
-
+#nested loops
+for line in range(1,10):
+    for x in range(line):
+        print (line, end = ' ')
+    print("")
+#controls where the pryrimid will start at
 x=0
-y=1
-for number in range(5):
-    print (x, end = ' ')
-    print (y, end = ' ')
-    x=x+y
-    y=x+y
+#controls where it wil finish
+#will break if number is over 10
+y=10
+for line in range(x,y):
+    #mkae sure each number is on a diffrent line
+    print()
+#both loops uses line to control flow
+    for space in range (9-line):
+        #prints spaces on first side
+        print(" ",end='')
+    for number in range (line):
+        #prints the first side numbers
+        print (line,end ='')
+    #prints space in between both sides
+    print (' ',end ='')
+    for number in range(line):
+        #prints the second set of numbers
+        print (line, end = '')
+print()
+print()
+
+#second triabgle
+x=0
+#controls where it wil finish
+#will break if number is over 10
+y=9
+for line in range(x,y):
+    #mkae sure each number is on a diffrent line
+    print()
+#both loops uses line to control flow
+    for space in range (9-line):
+        #prints spaces on first side
+        print(" ",end='')
+    for number in range (line, 0, -1):
+        #prints the first side numbers
+        print (number,end ='')
+    for number in range (line):
+        print(number,end = '')
---
+++ Unit 1/loopingFUN.py
@@ -0,0 +1,12 @@
+#Daniel Walker
+#having fun with loops
+#learn how to resize our programs
+#asking the usr for values
+#is requesting values via consle input
+#type casting
+begin = int(input('number of lines\n'))
+lines = begin
+for line in range(lines):
+    for number in range(begin-line,0,-1):
+        print(number, end = ' ')
+    print()
