94af8ba154eb66696a524972b08e59a33e2732bd
Dwalker1000 <25walkerd@gmail.com>
2020-09-25 11:37:01 -0500

    update

--- Unit 1/learning_Arrays.py
+++ Unit 1/learning_Arrays.py
@@ -1,5 +1,11 @@
 names = ['Ali', 'justin', 'Daniel', 'Jake', 'Rohan', 'Sarah', 'Augest', 'Aarian', 'Kunal', 'Safin']
 #          0       1         2         3       4        5        6          7        8        9
-print(names[2])
+print('list of names in class')
 for i in range (len(names)):
-    print(names[i])
+    print(names[i], end = ' ')
+print()
+print()
+print('list of diffrent foods')
+food = ['pizza', 'burger', 'pasta', 'fries']
+for i in range(len(food)):
+    print(food [i], end = ' ')
