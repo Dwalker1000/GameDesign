3dd9ed89f6445d637b6f6cfeb1207b0ae0e86dd5
Dwalker1000 <25walkerd@gmail.com>
2020-10-08 20:50:40 -0500

    change dict key in to commet becuse i was getting error

--- Unit 1/learning_Arrays.py
+++ Unit 1/learning_Arrays.py
@@ -128,5 +128,5 @@ myDictionary = {'name1':"Daniel", 'age1':15,'school1':"Greenhill"}
 print("The name of the student is",myDictionary['name1'])
 print("This person is ", myDictionary['age1'], "years old")
 print("He goes to ", myDictionary['school1'])
-print(myDictionary.key())
+# print(myDictionary.key())
 print()
