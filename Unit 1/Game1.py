6a65e9fa074e91ab5a728d36e999b2c27bc8c17b
Dwalker1000 <25walkerd@gmail.com>
2020-10-19 18:12:01 -0500

    program update not final

--- Unit 1/Game1.py
+++ Unit 1/Game1.py
@@ -3,10 +3,10 @@ import time
 
 name=input("What is your name? ")
 print("Good luck! ", name)
-gameWords=['python', 'java','PHP','javaScript', 'computer', 'geeks', 'keyboard','laptop', 'headphone', 'hardware','software' ]
+gameWords=['python', 'java','php','javascript', 'computer', 'geeks', 'keyboard', 'laptop', 'headphone', 'hardware','software', 'cpu', 'drivers', 'usb ports']
 # use the choice method of my random fucntion to pick a gameWords
 answer=input('do you want to guess a word ')
-
+print('only guess lower case letters')
 while answer == 'yes':
     word= random.choice(gameWords)
     guesses =''
@@ -14,12 +14,19 @@ while answer == 'yes':
     while turns>0:
         for char in word:
             if char in guesses:
-                print(char)
+                print(char,end = '')
             else:
-                print('_')
+                print('_', end = ' ')
+        print('')
         guess= input("give me a letter: ")
+        if guess in word:
+            break
+        else:
+            turns-=1
         guesses += guess
-        turns-=1
+        print(turns)
 
+        if turns == 0:
+            print('Game Over')
     answer=input('do you want to play again ')
 time.sleep(5)
