433b06884a79e927231316e9ec6834263e25e35a
Dwalker1000 <25walkerd@gmail.com>
2020-11-03 20:48:52 -0600

    update/ classwork

---
+++ Unit 1/CreatingScreen.py
@@ -0,0 +1,18 @@
+import pygame
+#drawing with python
+pygame.init() #first command
+screen = pygame.display.set_mode((800,600)) #this is a tuple
+screen.fill((0,0,0)) #select color of background 0-255
+pygame.display.flip() #update screen quickly also use .update
+pygame.display.set_caption("Testing pygame") #title for the window
+run = True
+while run == True:
+    pygame.time.delay(100)
+    screen.fill((162,39,142))
+    pygame.display.update()
+    for x in pygame.event.get():
+        print(x)
+        if x.type == pygame.QUIT:
+            run = False
+pygame.time.delay(50)
+pygame.quit()
--- Unit 1/FileSaveGame.py
+++ Unit 1/FileSaveGame.py
@@ -1,45 +1,45 @@
 import random
 import time
 import os
+import datetime
 def menu ():
-    print("***************************************************")
-    print("*                    Hang Man                     *")
-    print("*                                                 *")
-    print("*      Instructions: A word will be randomly      *")
-    print("*    selected from a list. your job is to guess   *")
-    print("*      the word picked from the list to win.      *")
-    print("*                                                 *")
-    print("*  1. play                                        *")
-    print("*  2. see past scores                             *")
-    print("*  3. Exit Game                                   *")
-    print("***************************************************")
+    print("***********************************************************")
+    print("*                         Hang Man                        *")
+    print("*                                                         *")
+    print("*          Instructions: A word will be randomly          *")
+    print("*        selected from a list. your job is to guess       *")
+    print("*          the word picked from the list to win.          *")
+    print("*                                                         *")
+    print("*  1. play                                                *")
+    print("*  2. see past scores (dont do if you havent played yet)  *")
+    print("*  3. Exit Game                                           *")
+    print("***********************************************************")
 def start ():
     start = input("(1,2,3) ")
     return start
 def play ():
-    global gameWords
+
     name=input("What is your name? ")
     print("Good luck!", name)
-
     path = "words.txt"
     ifExist = os.path.exists(path)
-    if ifExist == "True":
+
+    f = open('words.txt', 'w')
+    words = ['python', 'java', 'php', 'javascript', 'computer', 'geeks', 'keyboard', 'laptop', 'headphone', 'hardware', 'software', 'cpu', 'drivers', 'usb ports', 'binary', 'graphics', 'pixels', 'intel', 'ryzen', 'battery']
+    z = len(words)
+    for x in range (z):
+        y = words[x]
+        f.write(y+"\n")
+    f.close()
+
+    # use the choice method of my random fucntion to pick a gameWords
+    answer=input('do you want to guess a word? ')
+    while answer == 'yes':
         f = open('words.txt', 'r')
         gameWords = []
         for line in f:
             gameWords.append(line.strip())
         f.close()
-        return gameWords
-    elif ifExist == "False":
-        f = open('words.txt', 'w')
-        gameWords = ['python', 'java', 'php', 'javascript', 'computer', 'geeks', 'keyboard', 'laptop', 'headphone', 'hardware', 'software', 'cpu', 'drivers', 'usb ports', 'binary', 'graphics', 'pixels', 'intel', 'ryzen', 'battery']
-        f.write(gameWords)
-        f.close()
-        return gameWords
-
-    # use the choice method of my random fucntion to pick a gameWords
-    answer=input('do you want to guess a word? ')
-    while answer == 'yes':
         word= random.choice(gameWords)
         guesses =''
         final = ''
@@ -56,9 +56,10 @@ def play ():
             if word in final:
                 print('you win')
                 print("the word is: "+ word)
-                score += 1
-                print("you have a score of "+ score)
-                answer=input('do you want to keep playing ')
+                score = score + 1
+                print("you have a score of ",end = '')
+                print(score)
+                answer=input('do you want to guess another word ')
                 break
             guess= input("give me a letter: ")
             guesses += guess.lower()
@@ -220,17 +221,19 @@ def play ():
                 print('    ')
             if turns == 0:
                 print('Game Over')
+                print("you ended with a score of ", end ='')
+                print(score)
                 answer=input('do you want to play again ')
                 break
             path = "highscores.txt"
             ifExist = os.path.exists(path)
             if ifExist == "True":
                 text_file = open("highscores.txt", "a")
-                text_file.write("\n" + name + ' has a score of ' + score + "\n")
+                text_file.write("\n" + name +"\t"+ score + "\t" + date.today()+ "\n")
                 text_file.close()
             elif ifExist == "False":
                 text_file = open("highscores.txt", "w")
-                text_file.write("\n" + name + ' has a score of ' + score + "\n")
+                text_file.write("\n" + name +"\t"+ score + "\t" + date.today()+ "\n")
                 text_file.close()
 
 def score():
@@ -244,11 +247,12 @@ xyz = 0
 while xyz == 0:
     menu()
     x = start()
-    while x == '1':
+    if x == '1':
         play()
-    while x == '2':
+    if x == '2':
         score()
         break
     if x == '3':
         print('Thanks for playing! Goodbye.')
+        xyz = 1
         break
