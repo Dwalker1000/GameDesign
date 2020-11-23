8fbd764960e3f96e419d03ec5313b30e14df4975
Dwalker1000 <25walkerd@gmail.com>
2020-11-23 00:37:48 -0600

    game update/ More Sprites

--- Pygame/moving_Screen.py
+++ Pygame/moving_Screen.py
@@ -1,10 +1,12 @@
 import pygame
 pygame.init()
 #background pick should be size of Screen
-walkRight = [pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\pika.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\pika.png")]
-walkLeft = [pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\pika.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\pika.png")]
+walkRight = [pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkRight1.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkRight2.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkRight3.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkRight4.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkRight5.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkRight6.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkRight7.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkRight8.png")]
+walkLeft = [pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkLeft1.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkLeft2.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkLeft3.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkLeft4.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkLeft5.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkLeft6.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkLeft7.png"), pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\WalkLeft8.png")]
+jumpRight = pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\JumpRight.png")
+jumpLeft = pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\JumpLeft.png")
 background = pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\Backgrounds\\Elongated Background.jpg")
-character = pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\pika.png")
+character = pygame.image.load("C:\\Users\\walkerd24\\github\\GameDesign\\Pygame\\sprites\\Standing.png")
 WIDTH = 1000 # screen width
 HEIGHT = 750 # screen height
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -20,7 +22,7 @@ backy = 0 #background starting y pos
 #jump code/walk
 jump = False
 high = 10 #height of jump
-speed = 25
+speed = 25 #needs to be able to be devided by 500 cleanly or will mess up on far side
 # to control the frames
 clock = pygame.time.Clock()
 #control left and right move
@@ -29,7 +31,7 @@ right = False
 true = False
 #control list
 walkCount = 0
-SpriteFrames = 6 #each picture is about 3 frames and we have X pictures 3*x = number (27 in this case 3*9)
+SpriteFrames = 24 #each picture is about 3 frames and we have X pictures 3*x = number (27 in this case 3*9)
 
 def redrawWindow():
     KeyPress=pygame.key.get_pressed()
@@ -39,6 +41,10 @@ def redrawWindow():
     global x
     global speed
     global true
+    global jump
+    global jumpRight
+    global jumpLeft
+    #screen scrolling
     space = 0
     if x < 500 or x > 500:
         screen.blit(background, (backx,backy))
@@ -56,21 +62,26 @@ def redrawWindow():
     if x >= 2500 and space == 2000:
         screen.blit(background, (backx,backy))
 
+    #sprite movement code
     if walkCount + 1 >= SpriteFrames:
         walkCount = 0
-
     if left:
-        screen.blit(walkLeft[walkCount//3],(x,y))
-        walkCount += 1
-
+        if jump == True:
+            screen.blit((jumpLeft), (x,y))
+        else:
+            screen.blit(walkLeft[walkCount//3],(x,y))
+            walkCount += 1
     elif right:
-        screen.blit(walkRight[walkCount//3], (x,y))
-        walkCount +=1
-
+        if jump == True:
+            screen.blit((jumpRight), (x,y))
+        else:
+            screen.blit(walkRight[walkCount//3], (x,y))
+            walkCount +=1
     else:
         screen.blit((character),(x,y))
         walkCount = 0
 
+    #screen update
     pygame.display.update()
 
 while running:
